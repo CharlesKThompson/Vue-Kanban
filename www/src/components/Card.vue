f24fa09368d5b693d02d2e4b8b275723a17f733b
Bryce Murray <brycepmurray711@gmail.com>
2018-03-06 14:08:37 -0700

    components added to project(non filled)

---
+++ www/src/components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="card">
+    
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
---
+++ www/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                msg: 'Welcome To The Jungle!'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .home {
+        background-color: aliceblue;
+    }
+</style>
