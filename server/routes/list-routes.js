b5f6d78345ae721eb0ab13241b39d1715397bbfe
Charles <chuckness32@hotmail.com>
2018-03-06 11:50:53 -0700

    all routes written

---
+++ server/routes/list-routes.js
@@ -0,0 +1,48 @@
+var router = require("express").Router();
+var Lists = require("../models/list");
+
+//Create a list
+router.post("/lists", (req, res, next) => {
+    req.body.creatorId = req.session.uid
+
+    Lists.create(req.body)
+        .then(list => {
+            res.send(list)
+        })
+        .catch(next)
+})
+
+//Get a list
+router.get("/lists/:listid", (req, res, next) => {
+    Lists.findById(req.params.listid)
+        .then(list => {
+            return res.send(list)
+        })
+        .catch(next)
+})
+
+//Put a list
+router.put("/lists/:listid", (req, res, next) => {
+    Lists.findByIdAndUpdate(req.params.listid, req.body)
+        .then(list => {
+            res.send({ message: "Successfully updated list", data: list })
+        })
+        .catch(next)
+})
+
+//Delete a list
+router.delete("/lists/:listid", (req, res, next) => {
+    Lists.findOneAndRemove({ creatorId: req.session.uid, _uid: req.params.listid })
+        .then(list => {
+            if (!Lists) {
+                res.status(401).send({ error: "Not authorized to remove list" })
+            } else {
+                res.send({ message: "Successfully deleted list" })
+            }
+        })
+        .catch(next)
+})
+    
+
+
+module.exports = {router}
---
+++ server/routes/task-routes.js
@@ -0,0 +1,48 @@
+var router = require("express").Router();
+var Tasks = require("../models/task");
+
+//Create a task
+router.post("/tasks", (req, res, next) => {
+    req.body.creatorId = req.session.uid
+
+    Tasks.create(req.body)
+        .then(task => {
+            res.send(task)
+        })
+        .catch(next)
+})
+
+//Get a task
+router.get("/tasks/:taskid", (req, res, next) => {
+    Tasks.findById(req.params.taskid)
+        .then(task => {
+            return res.send(task)
+        })
+        .catch(next)
+})
+
+//Put a task
+router.put("/tasks/:taskid", (req, res, next) => {
+    Tasks.findByIdAndUpdate(req.params.taskid, req.body)
+        .then(task => {
+            res.send({ message: "Successfully updated task", data: task })
+        })
+        .catch(next)
+})
+
+//Delete a task
+router.delete("/tasks/:taskid", (req, res, next) => {
+    Tasks.findOneAndRemove({ creatorId: req.session.uid, _uid: req.params.taskid })
+        .then(task => {
+            if (!Tasks) {
+                res.status(401).send({ error: "Not authorized to remove task" })
+            } else {
+                res.send({ message: "Successfully deleted task" })
+            }
+        })
+        .catch(next)
+})
+    
+
+
+module.exports = {router}
