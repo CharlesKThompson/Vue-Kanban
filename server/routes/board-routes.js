0b1ba46e7831b45d7d3a46a8cfa3f247b3b7d2f2
Charles <chuckness32@hotmail.com>
2018-03-06 10:48:19 -0700

    delete board route

--- server/routes/board-routes.js
+++ server/routes/board-routes.js
@@ -6,28 +6,45 @@ router.post("/api/boards/", (req, res, next) => {
     req.body.creatorId = req.session.uid
 
     Boards.create(req.body)
-     .then(board => {
-         res.send(board)
-     })
-     .catch(next)
+        .then(board => {
+            res.send(board)
+        })
+        .catch(next)
 })
 
 //Get a board
 router.get("/api/boards/:boardid", (req, res, next) => {
     Boards.findById(req.params.boardid)
-    .then(board => {
-        return res.send(board)
-    })
-    .catch(next)
+        .then(board => {
+            return res.send(board)
+        })
+        .catch(next)
 })
 
 //Put a board
 router.put("/api/boards/:boardid", (req, res, next) => {
     Boards.findByIdAndUpdate(req.params.boardid, req.body)
         .then(board => {
-            res.send({message: "Successfully updated post", data: board})
+            res.send({ message: "Successfully updated post", data: board })
         })
         .catch(next)
-    })
+})
 
-    //Delet a board
+//Delet a board
+router.delete("/api/boards/:boardid", (req, res, next) => {
+    Boards.findByIdAndRemove({ creatorId: req.session.uid, _uid: req.params.boardid })
+        .then(post => {
+            if (!Boards) {
+                res.status(401).send({ error: "Not authorized to remove board" })
+            } else {
+                res.send({message: "Successfully deleted post"})
+                }
+                    .catch(next)
+                )
+            }
+        })
+    
+
+
+
+})
