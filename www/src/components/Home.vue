f235e6354a35ba1670a1d722127553d8cd33a503
Joe Davis <joecrush@msn.com>
2018-03-07 13:09:22 -0700

    Methods and board component added to Home.vue

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -1,26 +1,56 @@
 <template>
-  <div class="home">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-3" v-for = "board in boards">
+    <div class="home">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3" v-for="board in boards">
 
+                </div>
+                <button type="button" class="btn btn-danger" @click='logout'>Logout</button>
             </div>
-            <button type="button" class="btn btn-danger" @click='logout'>Logout</button>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
     import Navbar from './Navbar.vue'
+    import Board from './Board.vue'
     export default {
         name: 'Home',
+        mounted() {
+            this.$store.dispatch('getBoards')
+        },
         data() {
             return {}
         },
         components: {
             Navbar,
+            board
         },
+        methods: {
+            getBoards() {
+                this.$store.dispatch('getBoards')
+            },
+            addBoard(board) {
+                this.$store.dispatch('addBoard', board);
+            },
+            removeBoard(board) {
+                this.$store.dispatch('removeBoard', board)
+            },
+            updateBoard(board) {
+                this.$store.dipatch('updateBoard', board)
+            }
+        },
+        computed: {
+            boards() {
+                return this.$store.state.boards
+            },
+            user() {
+                return this.$store.state.user
+            },
+            activeBoard() {
+                return this.$store.state.activeBoard
+            }
+        }
     }
 </script>
 
