c8c28f80309cc71953a808e172b21cb7b75a63b1
Joe Davis <joecrush@msn.com>
2018-03-07 13:48:20 -0700

    some inital stuff for Board and List.ve

--- www/src/components/Board.vue
+++ www/src/components/Board.vue
@@ -1,12 +1,43 @@
 <template>
-<div class="board">
-    
-</div>
+    <div class="board">
+
+    </div>
 </template>
 
 <script>
+    // import List from './List.vue'
+    export default {
+        name: 'Board',
+        data() {
+            return {
+                boardId: this.$route.params.boardId,
+            }
+        },
+        // props: ['board'],
+        mounted() {
+            this.$store.dispatch('getBoards')
+        },
+        computed: {
+            lists() {
+                return this.$store.state.lists[this.board._id] || []
+            },
+            activeBoard() {
+                return this.$store.state.activeBoard
+            }
+        },
+        methods: {
+            addList(list) {
+                this.$store.dispatch('addList', list)
+            },
+            removeBoard(board) {
+                this.$store.dispatch('removeBoard', board)
+            }
+        },
+        components: {
+            // List
+        }
+    }
 </script>
 
 <style>
-
 </style>
---
+++ www/src/components/List.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="list">
+
+    </div>
+</template>
+
+<script>
+    // import List from './List.vue'
+    export default {
+        name: 'List',
+        data() {
+            return {
+
+            }
+        },
+        // props: ['list'],
+        mounted() {
+            // this.$store.dispatch('getLists')
+            // this.$store.dispatch('getTasks') 
+        },
+        computed: {
+            lists() {
+                return this.$store.state.lists[this.list._id] || []
+            }
+        },
+        methods: {
+            addTask() {
+                let task = {
+                    body: this.task,
+                    listId: this.list._id,
+                    boardId: this.list.boardId
+                }
+                console.log(task)
+                this.$store.dispatch('addTask', task);
+            },
+
+            removeList(list) {
+                this.$store.dispatch('removeList', list)
+            }
+        },
+        components: {
+            // List
+        }
+    }
+</script>
+
+<style>
+</style>
