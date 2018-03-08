c008ef2fd7b2ec04907d3c801e2cc06a208c3b6f
Joe Davis <joecrush@msn.com>
2018-03-08 06:04:02 -0700

    simple layout starting point

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -4,26 +4,34 @@
             <div class="row">
                 <!-- CREATE A BOARD FORM -->
                 <div class="col-sm-6">
-                    <form class="form-inline" @submit.prevent="addBoard">
+                    <form class="form-inline mtop" @submit.prevent="addBoard">
                         <input type="text" class="form-control" v-model="board.title" name="title" placeholder="Add a New Board">
+                        <!-- ADD BOARD BUTTON -->
                         <button class="btn btn-primary mleft">
                             Add New Board
                         </button>
+                        <!-- RESET BUTTON -->
                         <button class="btn btn-warning mleft" type="reset">Reset</button>
-                        <!-- LOGOUT BUTTON -->
-                        <button type="button" class="btn btn-danger mleft" @click='logout'>Logout</button>
                     </form>
                 </div>
             </div>
             <!-- BOARDS DRAW HERE -->
-            <div class="row board-home">
+            <div class="row">
                 <div class="col-sm-3 board-box-home" v-for="board in boards">
                     {{board.title}}
-                    <!-- DELETE BOARD BUTTON -->
+                    <!-- DELETE BOARD ICON-BUTTON -->
                     <button class="list-btn trash-board" @click.prevent='removeBoard(board)'>
                         <img class="trash-board" src="../assets/trash-small.png">
                     </button>
                 </div>
+            </div>
+            <!-- LOGOUT BUTTON -->
+            <div class="row justify-content-end">
+                <div class="col-sm-9"></div>
+                <div class="col-sm-2">
+                    <button type="button" class="btn btn-danger mleft logout mbottom" @click='logout'>Logout</button>
+                </div>
+                <div class="col-sm-1"></div>
             </div>
         </div>
     </div>
@@ -88,26 +96,40 @@
         background-color: white;
         color: black;
         padding: 1rem;
-        margin: 1rem;
+        margin: 2rem;
         outline: black;
         outline-style: solid;
         outline-width: 1px;
 
     }
 
-    .board-home {
+    /* .board-home {
         display: flex;
         flex-flow: column;
         height: 100%;
         min-height: 500px;
-    }
+    } */
 
     .trash-board {
         color: black;
         background: none;
         border: none;
     }
+
     .mleft {
         margin-left: .2rem;
     }
+
+    .logout {
+        margin-right: 2rem;
+    }
+
+    .mtop {
+        margin-top: 1rem;
+    }
+
+    .mbottom {
+        margin-bottom: 1rem;
+    }
+
 </style>
