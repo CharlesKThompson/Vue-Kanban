0c6157becc195e46bd7a31ab100fec316d9c8172
Joe Davis <joecrush@msn.com>
2018-03-09 13:27:36 -0700

    Logo updated to be smaller. Added to Navbar component

--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="navbar">    
-    
+    <div class="container-fluid">
+        <img src="../assets/listify-logo-sm.png" alt="Listify"> Hello World
+    </div>
 </div>
 </template>
 
